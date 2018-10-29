63b1fd18c942a43b4a846a3bd2e3bfb13d086fdf
Evgeny Fedorov <evgeny.fedorov2@gmail.com>
2018-10-29 20:17:52 +0100

    have all call styles in a separate function

--- week 1/index.js
+++ week 1/index.js
@@ -1,24 +1,22 @@
 'use strict';
 
+const request = require('request-promise');
 const BASE_URL = 'http://swapi.co/api/people/1';
 
-//// ------ Callback style ------------------------------------------------------
-/*
-const request = require('request');
-request(BASE_URL, (error, response, body) => {
-    const vehicles = JSON.parse(body).vehicles;
-    vehicles.forEach((url) => {
-        request(url, (error, response, body) => { 
-            console.log(JSON.parse(body).name);
+function callbackStyle () {
+    const request = require('request');
+    request(BASE_URL, (error, response, body) => {
+        const vehicles = JSON.parse(body).vehicles;
+        vehicles.forEach((url) => {
+            request(url, (error, response, body) => { 
+                console.log(JSON.parse(body).name);
+            });
         });
     });
-});
-*/
+}
 
-const request = require('request-promise');
-// ------ Promise style -------------------------------------------------------
-/*
-request(BASE_URL)
+function promiseStyle () {
+    request(BASE_URL)
     .then((body) => {
         const vehicles = JSON.parse(body).vehicles;
         vehicles.forEach(url => {
@@ -34,9 +32,9 @@ request(BASE_URL)
     .catch(err => {
         console.error(err, 'Something went wrong');
     })
-*/
+}
 
-async function run() {
+async function asyncStyle () {
     const result = await request(BASE_URL);
     const vehicles = JSON.parse(result).vehicles;
     vehicles.forEach(async (url) => {
@@ -45,4 +43,6 @@ async function run() {
     });
 }
 
-run();
+//callbackStyle()
+//promiseStyle();
+asyncStyle();
